fe8e265c6abb2a2f2a1f3c8d513734891de906c0
DmitriyYasilonis <51056074+DmitriyYasilonis@users.noreply.github.com>
2020-02-17 19:07:35 +0200

    Task 9

---
+++ src/Module2TAsk9Accounting.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Module2TAsk9Accounting {
+    static double basePay = 1.5;
+    static int hoursWorked = 10;
+
+    public static void pay() {
+        double totalSalary = 0;
+        if (hoursWorked > 1) {
+            totalSalary = basePay * hoursWorked;
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("Your total salary is " + totalSalary);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Base Pay:");
+        basePay = in.nextDouble();
+        System.out.println("Enter Hours Worked:");
+        hoursWorked = in.nextInt();
+        pay();
+    }
+}

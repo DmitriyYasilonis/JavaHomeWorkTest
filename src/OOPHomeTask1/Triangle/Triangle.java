4cad7239cff1bad08409a3157df20ad0040ef9fb
DmitriyYasilonis <51056074+DmitriyYasilonis@users.noreply.github.com>
2020-05-17 16:27:06 +0300

    edit Triangle

--- src/OOPHomeTask1/Triangle/Triangle.java
+++ src/OOPHomeTask1/Triangle/Triangle.java
@@ -1,16 +1,38 @@
 package OOPHomeTask1.Triangle;
 
 public class Triangle {
-    public static double area(Point A, Point B, Point C) throws TriangleException {
+    private  Point a,b,c;
 
-        if ((A.equals(B) || A.equals(C) || B.equals(C)) ||
-                (A.x == B.x && A.x == C.x) ||
-                (A.y == B.y && A.y == C.y)) {
+    public Triangle (Point A, Point B, Point C){
+        this.a = A;
+        this.b = B;
+        this.c = C;
+        }
+    public double area() throws TriangleException {
+
+        if ((a.equals(b) || a.equals(c) || b.equals(c)) ||
+                (a.x == b.x && a.x == c.x) ||
+                (a.y == b.y && a.y == c.y)) {
             throw new TriangleException();
         }
 
-        double area = (A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y)) / 2;
+        double area = (a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) / 2;
         return Math.abs(area);
 
     }
 }
+
+//public class Triangle {
+//    public static double area(Point A, Point B, Point C) throws TriangleException {
+//
+//        if ((A.equals(B) || A.equals(C) || B.equals(C)) ||
+//                (A.x == B.x && A.x == C.x) ||
+//                (A.y == B.y && A.y == C.y)) {
+//            throw new TriangleException();
+//        }
+//
+//        double area = (A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y)) / 2;
+//        return Math.abs(area);
+//
+//    }
+//}
